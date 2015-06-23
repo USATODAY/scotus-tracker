2f6237b9e7d7763c9ffb91b04e2f3fd003c4deae
SJS <shawnjsullivan@gmail.com>
2015-06-23 09:44:48 -0400

    Several changes requested by Politics
    Including: New data.json, broken up first and last names in justice.js,
    modifications to CSS to increase padding, removing logo from splash,
    and a few semantic changes on the templates.

--- src/js/models/justices.js
+++ src/js/models/justices.js
@@ -6,39 +6,48 @@ define([
             this.justices = [
                 {
                     "name": "John G. Roberts",
-                    "last_name": "Roberts"
+                    "last_name": "Roberts",
+                    "first_name":"John G."
                 },
                 {
                     "name": "Antonin Scalia",
-                    "last_name": "Scalia"
+                    "last_name": "Scalia",
+                    "first_name":"Antonin"
                 },
                 {
                     "name": "Anthony Kennedy",
-                    "last_name": "Kennedy"
+                    "last_name": "Kennedy",
+                    "first_name":"Anthony"
                 },
                 {
                     "name": "Clarence Thomas",
-                    "last_name": "Thomas"
+                    "last_name": "Thomas",
+                    "first_name":"Clarence"
                 },
                 {
                     "name": "Ruth Bader Ginsburg",
-                    "last_name": "Ginsburg"
+                    "last_name": "Ginsburg",
+                    "first_name":"Ruth Bader"
                 },
                 {
                     "name": "Stephen Breyer",
-                    "last_name": "Breyer"
+                    "last_name": "Breyer",
+                    "first_name":"Stephen"
                 },
                 {
                     "name": "Samuel Alito",
-                    "last_name": "Alito"
+                    "last_name": "Alito",
+                    "first_name":"Samuel"
                 },
                 {
                     "name": "Sonia Sotomayor",
-                    "last_name": "Sotomayor"
+                    "last_name": "Sotomayor",
+                    "first_name":"Sonia"
                 },
                 {
                     "name": "Elena Kagan",
-                    "last_name": "Kagan"
+                    "last_name": "Kagan",
+                    "first_name":"Elena"
                 }
 
             ];
--- src/js/templates.js
+++ src/js/templates.js
@@ -6,7 +6,7 @@ this["templates"]["app.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<div class="iapp-panel iapp-intro-panel">\n    <div class="intro-box">\n        <div class="logo"><img src="http://www.gannett-cdn.com/experiments/usatoday/2015/06/scotus-tracker/img/logo.svg" width="42%"/></div>\n        <h1 class="iapp-page-header">' +
+__p += '<div class="iapp-panel iapp-intro-panel">\n    <div class="intro-box">\n        \n        <h1 class="iapp-page-header">' +
 ((__t = (head)) == null ? '' : __t) +
 '</h1>\n\n        <p class="iapp-page-chatter">' +
 ((__t = (chatter)) == null ? '' : __t) +
@@ -21,7 +21,7 @@ obj || (obj = {});
 var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 with (obj) {
-__p += '<div class="iapp-case-wrap">\n    <div class="small-head center">Scotus decision tracker</div>\n    <div class="case-bar-wrap">\n        <h2 class="iapp-case-name">' +
+__p += '<div class="iapp-case-wrap">\n    <div class="small-head center">Supreme Court Decision Tracker</div>\n    <div class="case-bar-wrap">\n        <h2 class="iapp-case-name">' +
 ((__t = (case_title)) == null ? '' : __t) +
 ' </h2>\n\n        <p class="iapp-decided-indicator">' +
 ((__t = (case_name )) == null ? '' : __t) +
@@ -46,35 +46,41 @@ __p += '\n                    ';
 __p += '\n                    <td class="iapp-for"><img src="http://www.gannett-cdn.com/experiments/usatoday/2015/06/scotus-tracker/img/' +
 ((__t = (justice.name.substr(justice.name.lastIndexOf(" ") + 1, justice.name.length - justice.name.lastIndexOf(" ") + 1).toLowerCase() )) == null ? '' : __t) +
 '.jpg" width="100%"/>\n                        ' +
-((__t = (justice.name )) == null ? '' : __t) +
-'<br/>MAJORITY</td>\n                    ';
+((__t = (justice.first_name)) == null ? '' : __t) +
+'<br/>' +
+((__t = (justice.last_name)) == null ? '' : __t) +
+'<br/><br/><span class="vote">MAJORITY</span></td>\n                    ';
 } else if(justice.status == "in-part") { ;
 __p += '\n                    <td class="iapp-in-part"><img src="http://www.gannett-cdn.com/experiments/usatoday/2015/06/scotus-tracker/img/' +
 ((__t = (justice.name.substr(justice.name.lastIndexOf(" ") + 1, justice.name.length - justice.name.lastIndexOf(" ") + 1).toLowerCase() )) == null ? '' : __t) +
 '.jpg" width="100%"/>\n                        ' +
-((__t = (justice.name )) == null ? '' : __t) +
-'<br/>CONCUR IN PART</td>\n                    ';
+((__t = (justice.first_name)) == null ? '' : __t) +
+'<br/>' +
+((__t = (justice.last_name)) == null ? '' : __t) +
+'<br/><br/><span class="vote">CONCUR</span></td>\n                    ';
 } else if(justice.status == "against") { ;
 __p += '\n                    <td class="iapp-against"><img src="http://www.gannett-cdn.com/experiments/usatoday/2015/06/scotus-tracker/img/' +
 ((__t = (justice.name.substr(justice.name.lastIndexOf(" ") + 1, justice.name.length - justice.name.lastIndexOf(" ") + 1).toLowerCase() )) == null ? '' : __t) +
 '.jpg" width="100%"/>\n                        ' +
-((__t = (justice.name )) == null ? '' : __t) +
-'<br/>DISSENT</td>\n                    ';
+((__t = (justice.first_name)) == null ? '' : __t) +
+'<br/>' +
+((__t = (justice.last_name)) == null ? '' : __t) +
+'<br/><br/><span class="vote">DISSENT</span></td>\n                    ';
 };
 __p += '\n\n                    ';
 });
-__p += '\n                </tr>\n            </table>\n\n        </div>\n    </div>\n    <div class="iapp-row-wrapper">\n        <div class="iapp-case-argument iapp-case-split-column">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-argument-title iapp-case-section-label">Argument For</h3>\n                <h4 class="iapp-case-argument-text">' +
-((__t = ( argument_for )) == null ? '' : __t) +
-'</h4>\n            </div>\n\n\n        </div>\n\n        <div class="iapp-case-argument iapp-case-split-column right">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-argument-title iapp-case-section-label">Argument Against</h3>\n                <h4 class="iapp-case-argument-text">' +
-((__t = ( argument_against )) == null ? '' : __t) +
-'</h4>\n            </div>\n        </div>\n    </div>\n    <div class="iapp-row-wrapper">\n        <div class="iapp-case-split-column">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Quote from Majority</h3>\n\n                <h4 class="iapp-case-quote">' +
+__p += '\n                </tr>\n            </table>\n\n        </div>\n    </div>\n        <div class="iapp-row-wrapper">\n        <div class="iapp-case-split-column">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Opinion</h3>\n\n                <h4 class="iapp-case-quote">' +
 ((__t = ( quote_1 )) == null ? '' : __t) +
 '</h4>\n\n                <h4 class="iapp-case-quote-source">' +
 ((__t = ( quote_1_source)) == null ? '' : __t) +
-'</h4>\n            </div>\n        </div>\n        <div class="iapp-case-split-column right">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Quote from dissent</h3>\n\n                <h4 class="iapp-case-quote">' +
+'</h4>\n            </div>\n        </div>\n        <div class="iapp-case-split-column right">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Dissent</h3>\n\n                <h4 class="iapp-case-quote">' +
 ((__t = ( quote_2 )) == null ? '' : __t) +
 '</h4>\n\n                <h4 class="iapp-case-quote-source">' +
 ((__t = ( quote_2_source)) == null ? '' : __t) +
+'</h4>\n            </div>\n        </div>\n    </div>\n\n    <div class="iapp-row-wrapper">\n        <div class="iapp-case-argument iapp-case-split-column">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-argument-title iapp-case-section-label">Argument For</h3>\n                <h4 class="iapp-case-argument-text">' +
+((__t = ( argument_for )) == null ? '' : __t) +
+'</h4>\n            </div>\n\n\n        </div>\n\n        <div class="iapp-case-argument iapp-case-split-column right">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-argument-title iapp-case-section-label">Argument Against</h3>\n                <h4 class="iapp-case-argument-text">' +
+((__t = ( argument_against )) == null ? '' : __t) +
 '</h4>\n            </div>\n        </div>\n    </div>\n\n    <div class="iapp-row-wrapper">\n        <div class="iapp-share-buttons">\n            <a href="https://twitter.com/intent/tweet?url=' +
 ((__t = (twitterShare)) == null ? '' : __t) +
 '&text=' +
@@ -112,28 +118,16 @@ obj || (obj = {});
 var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 with (obj) {
-__p += '<div class="iapp-case-wrap">\n    <div class="small-head center">Scotus decision tracker</div>\n    <div class="case-bar-wrap">\n        <h2 class="iapp-case-name">' +
+__p += '<div class="iapp-case-wrap">\n    <div class="small-head center">Supreme Court Decision Tracker</div>\n    <div class="case-bar-wrap">\n        <h2 class="iapp-case-name">' +
 ((__t = (case_title)) == null ? '' : __t) +
 ' </h2>\n\n        <p class="iapp-decided-indicator">' +
 ((__t = (case_name )) == null ? '' : __t) +
-' <span\n                class="label label-warning">(Undecided) Argued: ' +
+' <span\n                class="label label-warning">Argued: ' +
 ((__t = (case_date)) == null ? '' : __t) +
-'</span>\n        </p>\n    </div>\n    <div class="iapp-row-wrapper">\n        <div class="iapp-case-split-column">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Question</h3>\n                <h4 class="iapp-case-question">' +
+' (Undecided)</span>\n        </p>\n    </div>\n    <div class="iapp-row-wrapper">\n        <div class="iapp-case-split-column">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Question</h3>\n                <h4 class="iapp-case-question">' +
 ((__t = (question)) == null ? '' : __t) +
-'</h4>\n            </div>\n        </div>\n        <div class="iapp-case-split-column right">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Projected Outcome</h3>\n                <h4 class="iapp-outcome">' +
+'</h4>\n            </div>\n        </div>\n        <div class="iapp-case-split-column right">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Possible Outcome</h3>\n                <h4 class="iapp-outcome">' +
 ((__t = (outcome)) == null ? '' : __t) +
-'</h4>\n            </div>\n        </div>\n    </div>\n\n    <div class="iapp-row-wrapper">\n        <div class="iapp-case-argument iapp-case-split-column">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-argument-title iapp-case-section-label">Argument For</h3>\n                <h4 class="iapp-case-argument-text">' +
-((__t = ( argument_for )) == null ? '' : __t) +
-'</h4>\n            </div>\n\n\n        </div>\n\n        <div class="iapp-case-argument iapp-case-split-column right">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-argument-title iapp-case-section-label">Argument Against</h3>\n                <h4 class="iapp-case-argument-text">' +
-((__t = ( argument_against )) == null ? '' : __t) +
-'</h4>\n            </div>\n        </div>\n    </div>\n    <div class="iapp-row-wrapper">\n        <div class="iapp-case-split-column">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Quote For</h3>\n\n                <h4 class="iapp-case-quote">' +
-((__t = ( quote_1 )) == null ? '' : __t) +
-'</h4>\n\n                <h4 class="iapp-case-quote-source">' +
-((__t = ( quote_1_source)) == null ? '' : __t) +
-'</h4>\n            </div>\n        </div>\n        <div class="iapp-case-split-column right">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Quote Against</h3>\n\n                <h4 class="iapp-case-quote">' +
-((__t = ( quote_2 )) == null ? '' : __t) +
-'</h4>\n\n                <h4 class="iapp-case-quote-source">' +
-((__t = ( quote_2_source)) == null ? '' : __t) +
 '</h4>\n            </div>\n        </div>\n    </div>\n\n    <div class="iapp-row-wrapper">\n        <div class="iapp-case-full-column">\n            <h3 class="iapp-case-section-label">Decision Breakdown:</h3>\n            ';
  _.each(justices, function(justice, index) { ;
 __p += '\n            ';
@@ -147,10 +141,24 @@ __p += '\n            <table class="table iapp-decision-table">\n               
 __p += '\n                    <td class="iapp-undecided"><img src="http://www.gannett-cdn.com/experiments/usatoday/2015/06/scotus-tracker/img/' +
 ((__t = (justice.name.substr(justice.name.lastIndexOf(" ") + 1, justice.name.length - justice.name.lastIndexOf(" ") + 1).toLowerCase() )) == null ? '' : __t) +
 '.jpg" width="100%"/>\n                        ' +
-((__t = (justice.name )) == null ? '' : __t) +
-'<br/>\n                    ?</td>\n                    ';
+((__t = (justice.first_name)) == null ? '' : __t) +
+'<br/>' +
+((__t = (justice.last_name)) == null ? '' : __t) +
+'<br/>\n                    </td>\n                    ';
 });
-__p += '\n                </tr>\n            </table>\n\n        </div>\n    </div>\n\n    <div class="iapp-row-wrapper">\n        <div class="iapp-share-buttons">\n            <a href="https://twitter.com/intent/tweet?url=' +
+__p += '\n                </tr>\n            </table>\n\n        </div>\n    </div>\n\n    <div class="iapp-row-wrapper">\n        <div class="iapp-case-split-column">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Oral Argument</h3>\n\n                <h4 class="iapp-case-quote">' +
+((__t = ( quote_1 )) == null ? '' : __t) +
+'</h4>\n\n                <h4 class="iapp-case-quote-source">' +
+((__t = ( quote_1_source)) == null ? '' : __t) +
+'</h4>\n            </div>\n        </div>\n        <div class="iapp-case-split-column right">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-section-label">Oral Argument</h3>\n\n                <h4 class="iapp-case-quote">' +
+((__t = ( quote_2 )) == null ? '' : __t) +
+'</h4>\n\n                <h4 class="iapp-case-quote-source">' +
+((__t = ( quote_2_source)) == null ? '' : __t) +
+'</h4>\n            </div>\n        </div>\n    </div>\n    <div class="iapp-row-wrapper">\n        <div class="iapp-case-argument iapp-case-split-column">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-argument-title iapp-case-section-label">Argument For</h3>\n                <h4 class="iapp-case-argument-text">' +
+((__t = ( argument_for )) == null ? '' : __t) +
+'</h4>\n            </div>\n\n\n        </div>\n\n        <div class="iapp-case-argument iapp-case-split-column right">\n            <div class="iapp-case-section">\n                <h3 class="iapp-case-argument-title iapp-case-section-label">Argument Against</h3>\n                <h4 class="iapp-case-argument-text">' +
+((__t = ( argument_against )) == null ? '' : __t) +
+'</h4>\n            </div>\n        </div>\n    </div>\n\n\n\n\n    <div class="iapp-row-wrapper">\n        <div class="iapp-share-buttons">\n            <a href="https://twitter.com/intent/tweet?url=' +
 ((__t = (twitterShare)) == null ? '' : __t) +
 '&text=' +
 ((__t = (encodedShare)) == null ? '' : __t) +
